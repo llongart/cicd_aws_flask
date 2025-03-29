1628ff7c7b1b71194687ca174a5bf8cba17453fb
llongart <llongart413@gmail.com>
2025-03-29 13:36:05 -0400

    Feat: add endpoint '/api/foo'

--- app.py
+++ app.py
@@ -12,5 +12,6 @@ def hello():
 def data():
     return jsonify({"data": [1, 2, 3, 4, 5]})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/api/foo', methods=['GET'])
+def foo():
+    return jsonify({"message": "Hello, Foo"})
